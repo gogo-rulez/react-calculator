037a47b1054bfcc958e51aa3cd12ed8ae7ac2295
goran.savic <goran.savic@styria.hr>
2021-02-22 09:33:04 +0100

    fix markup in caluclator template

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -97,45 +97,43 @@ class Calculator extends Component {
 
 
         return (
-            <div className="App">
 
-                <div className="calculator">
+            <div className="calculator">
 
-                    <div className="calculator__screen">
-                        <span className="calculator__expression">
-                            { this.state.expression.map((char, index) => {
-                                return  <span key={index}>{ char }</span>
-                            }) }
-                        </span>
-                        <span className="calculator__result">
-                            { resultMarkup() }
-                        </span>
-                    </div>
-
-                    <div className="calculator__field calculator__field--add" onClick={() => this.updateExpression('+')}>+</div>
-                    <div className="calculator__field calculator__field--subtract" onClick={() => this.updateExpression('-')}>-</div>
-                    <div className="calculator__field calculator__field--multiply" onClick={() => this.updateExpression('*')}>*</div>
-                    <div className="calculator__field calculator__field--divide" onClick={() => this.updateExpression('/')}>/</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(1)}>1</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(2)}>2</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(3)}>3</div>
-                    <div className="calculator__field calculator__field--open_bracket" onClick={() => this.updateExpression('(')}>(</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(4)}>4</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(5)}>5</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(6)}>6</div>
-                    <div className="calculator__field calculator__field--close_bracket" onClick={() => this.updateExpression(')')}>)</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(7)}>7</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(8)}>8</div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(9)}>9</div>
-                    <div className="calculator__field calculator__field--equals" onClick={this.calculateResult}> = </div>
-                    <div className="calculator__field calculator__field--move_left" onClick={() => this.moveCursor('left')}> &lt; </div>
-                    <div className="calculator__field" onClick={() => this.updateExpression(0)}>0</div>
-                    <div className="calculator__field calculator__field--move_right" onClick={() => this.moveCursor('right')}> &gt; </div>
-                    <div className="calculator__field calculator__field--clear" onClick={() => this.resetExpression()}> C </div>
-
+                <div className="calculator__screen">
+                    <span className="calculator__expression">
+                        { this.state.expression.map((char, index) => {
+                            return  <span key={index}>{ char }</span>
+                        }) }
+                    </span>
+                    <span className="calculator__result">
+                        { resultMarkup() }
+                    </span>
                 </div>
 
+                <div className="calculator__field calculator__field--add" onClick={() => this.updateExpression('+')}>+</div>
+                <div className="calculator__field calculator__field--subtract" onClick={() => this.updateExpression('-')}>-</div>
+                <div className="calculator__field calculator__field--multiply" onClick={() => this.updateExpression('*')}>*</div>
+                <div className="calculator__field calculator__field--divide" onClick={() => this.updateExpression('/')}>/</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(1)}>1</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(2)}>2</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(3)}>3</div>
+                <div className="calculator__field calculator__field--open_bracket" onClick={() => this.updateExpression('(')}>(</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(4)}>4</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(5)}>5</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(6)}>6</div>
+                <div className="calculator__field calculator__field--close_bracket" onClick={() => this.updateExpression(')')}>)</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(7)}>7</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(8)}>8</div>
+                <div className="calculator__field" onClick={() => this.updateExpression(9)}>9</div>
+                <div className="calculator__field calculator__field--equals" onClick={this.calculateResult}> = </div>
+                <div className="calculator__field calculator__field--move_left" onClick={() => this.moveCursor('left')}> &lt; </div>
+                <div className="calculator__field" onClick={() => this.updateExpression(0)}>0</div>
+                <div className="calculator__field calculator__field--move_right" onClick={() => this.moveCursor('right')}> &gt; </div>
+                <div className="calculator__field calculator__field--clear" onClick={() => this.resetExpression()}> C </div>
+
             </div>
+
         );
 
     }
